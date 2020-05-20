3dac5129a0ee346a76bd2f30326c4837f1327000
Archaeologist03 <omniiknight@hotmail.com>
2020-05-20 15:21:10 +0200

    Adding code formatting - prettier/husky/lint-stagged.

--- src/components/CountrySwitcher/CountrySwitcher.jsx
+++ src/components/CountrySwitcher/CountrySwitcher.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 const CountrySwitcher = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  return <div></div>;
+};
 
-export default CountrySwitcher
+export default CountrySwitcher;
